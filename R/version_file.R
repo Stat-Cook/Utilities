c1b7b1d7364925623aaa2b289133e8a254360c99
Stat-Cook <51404549+Stat-Cook@users.noreply.github.com>
2023-12-14 12:53:47 +0000

    Update version_file.R

--- R/version_file.R
+++ R/version_file.R
@@ -1,8 +1,23 @@
 version_file <- function(prefix="Temp"){
   date.now <- as.Date(now())
-  key <- paste0(sample(c(letters[1:6[]], 0:9), 16), collapse="")
+  .bool <- TRUE
+  
+  while(.bool){  
+    key <- paste0(sample(c(letters[1:6[]], 0:9), 16), collapse="")
+    
+    .file <- as.character(glue::glue(
+      "{prefix}_{date.now}_{key}"
+    ))
+    
+    .bool <- file.exists(.file)
+  }
+  
+  .file
 
-  as.character(glue::glue(
-    "{prefix}_{date.now}_{key}"
-  ))
+}
+
+writeRDS_version <- function(object, prefix="Temp"){
+  .version_file <- version_file(prefix)
+  saveRDS(object, .version_file)
+  .version_file
 }
